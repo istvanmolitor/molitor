17586a87b17da6042c93edf634fe875001e884e6
Molitor István <istvan.molitor@centralmediacsoport.hu>
2026-03-04 16:43:32 +0100

    install

---
+++ install.sh
@@ -0,0 +1,28 @@
+mkdir packages
+cd packages
+git clone git@github.com:istvanmolitor/admin.git
+git clone git@github.com:istvanmolitor/article-parser.git
+git clone git@github.com:istvanmolitor/article-scraper.git
+git clone git@github.com:istvanmolitor/cms.git
+git clone git@github.com:istvanmolitor/cms-search.git
+git clone git@github.com:istvanmolitor/html-parser.git
+git clone git@github.com:istvanmolitor/language.git
+git clone git@github.com:istvanmolitor/media.git
+git clone git@github.com:istvanmolitor/menu.git
+git clone git@github.com:istvanmolitor/rss-watcher.git
+git clone git@github.com:istvanmolitor/setting.git
+git clone git@github.com:istvanmolitor/theme.git
+git clone git@github.com:istvanmolitor/user.git
+
+cd ../resources/js
+mkdir packages
+cd packages
+
+git clone git@github.com:istvanmolitor/vue-admin.git
+git clone git@github.com:istvanmolitor/vue-article-scraper.git
+git clone git@github.com:istvanmolitor/vue-cms.git
+git clone git@github.com:istvanmolitor/vue-media.git
+git clone git@github.com:istvanmolitor/ts-menu.git
+git clone git@github.com:istvanmolitor/vue-rss-watcher.git
+git clone git@github.com:istvanmolitor/vue-theme.git
+git clone git@github.com:istvanmolitor/vue-user.git

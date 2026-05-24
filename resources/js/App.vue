64a89e8cfebf5b423cebdc9c8fb53d9926b39e68
Molitor István <istvan.molitor@centralmediacsoport.hu>
2026-05-24 21:11:05 +0200

    Loading

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,10 +1,39 @@
 <template>
   <RouterView />
+  <LoadingSpinner v-if="isPageLoading" fullscreen overlay label="Oldal betoltese..." />
 </template>
 
-<script setup>
-</script>
+<script setup lang="ts">
+import { onMounted, onUnmounted, ref } from 'vue'
+import { useRouter } from 'vue-router'
+import LoadingSpinner from '@admin/components/ui/LoadingSpinner.vue'
 
+const router = useRouter()
+const isPageLoading = ref(true)
+
+const removeBeforeResolveGuard = router.beforeResolve(() => {
+  isPageLoading.value = true
+})
+
+const removeAfterEachHook = router.afterEach(() => {
+  isPageLoading.value = false
+})
+
+const removeErrorHook = router.onError(() => {
+  isPageLoading.value = false
+})
+
+onMounted(async () => {
+  await router.isReady()
+  isPageLoading.value = false
+})
+
+onUnmounted(() => {
+  removeBeforeResolveGuard()
+  removeAfterEachHook()
+  removeErrorHook()
+})
+</script>
 
 <style>
 /* Global styles - ensure full height */
@@ -12,4 +41,3 @@
   height: 100%;
 }
 </style>
-

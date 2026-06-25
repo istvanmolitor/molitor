e6d577e56acd269e07c04873fde44e6715ebdfb4
Molitor István <istvanmolitor@gmail.com>
2026-06-25 20:45:53 +0200

    gallery

--- resources/js/packages/vue-gallery/index.ts
+++ resources/js/packages/vue-gallery/index.ts
@@ -1,3 +1,6 @@
 export * from './services/galleryService'
 export * from './router'
 export { GalleryMenuBuilder, galleryMenuBuilder } from './config/menuBuilder'
+
+// CMS content element type registration
+import './registry/index'
